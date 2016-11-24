8fcd583dcbab822c7ae6fb085d62b5db497bcd09
Mallikarjun Yelameli <mallikarjun.yelameli@live.com>
2016-11-24 22:38:07 +0900

    Add files via upload

---
+++ featureNormalize.m
@@ -0,0 +1,22 @@
+function [norm]=featureNormalize(data)
+X=data(:,2:end);
+IDX=data(:,1);
+
+%function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+mu = mean(X);
+X_norm = bsxfun(@minus, X, mu);
+
+sigma = std(X_norm);
+X_norm = bsxfun(@rdivide, X_norm, sigma);
+
+norm=[IDX X_norm];
+end
+
+% ============================================================
+
+
